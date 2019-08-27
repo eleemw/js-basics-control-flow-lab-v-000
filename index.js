5fa4febba77d86f95939114e69dc952dc2b5b87c
Erica Murdock-Waters <ericahpa@gmail.com>
2019-08-27 04:14:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 function scuberGreetingForFeet(someValue) {
-  if (someValue <= 400) {
-     return 'This one is on me!'
+  if (someValue > 2500) {
+     return 'No can do.'
   }
-    else {
-    return 'I will gladly take your thirty bucks.'
+    else if (someValue > 2000) {
+      return 'I will gladly take your thirty bucks.'
   }
-
+      else if (someValue <= 400) {
+        return 'This ones on me!'
+  }
 }
